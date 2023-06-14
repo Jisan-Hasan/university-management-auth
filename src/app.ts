f885b9b5a8b28a904471047b36fe11030b335136
Jisan-Hasan <jisan.jh5@gmail.com>
2023-06-14 19:06:46 +0600

    [Done] Logger setup completed

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// console.log(app.get('env'));
+
 // Application Routes
 app.use('/api/v1/users/', usersRouter)
 
